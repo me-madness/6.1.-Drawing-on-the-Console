c53e66a26d560a6ef76aedf25fb0f8e99bb4fa55
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-01 22:25:47 +0200

    line

--- 09. House.py
+++ 09. House.py
@@ -6,4 +6,8 @@ if stars % 2 == 0:
     stars += 1
     
 roof_leght = math.ceil(n/2)
-padding_roof = (n - stars) / 2
+padding_roof = (n - stars) // 2
+line = '-' * padding_roof \
+      + '*' * stars \
+      + '-' * padding_roof
+print(line)            
