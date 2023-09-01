f719b9758b1b8901955359aebd3971b4416f4879
me-madness <58656549+me-madness@users.noreply.github.com>
2023-09-01 22:29:16 +0200

    for range

--- 09. House.py
+++ 09. House.py
@@ -5,9 +5,13 @@ stars = 1
 if stars % 2 == 0:
     stars += 1
     
-roof_leght = math.ceil(n/2)
-padding_roof = (n - stars) // 2
-line = '-' * padding_roof \
-      + '*' * stars \
-      + '-' * padding_roof
-print(line)            
+roof_leght = math.ceil(n / 2)
+for i in range(roof_leght):
+    padding_roof = (n - stars) // 2
+    line = '-' * padding_roof \
+          + '*' * stars \
+          + '-' * padding_roof
+    print(line) 
+    stars += 2
+    
+           
